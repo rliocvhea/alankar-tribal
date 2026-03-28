5f702f5105d562e40ead38b30184e2f643a752d2
Ravi Kumar <ravi.kumar@csharptek.com>
2026-03-28 14:35:59 +0530

    Remove PORT default

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ import orderRoutes from './routes/orders.js';
 import userRoutes from './routes/users.js';
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
